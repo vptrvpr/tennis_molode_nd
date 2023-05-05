b7dad0437cd833babf92f513b409c585cc1ce666
vptrvpr <vptrv.pr@gmail.com>
2023-05-05 19:57:14 +0300

    fix

--- resources/js/pages/reservation.vue
+++ resources/js/pages/reservation.vue
@@ -7,9 +7,12 @@
             <h6>Запись на муниципальные теннисные корты</h6>
           </div>
           <div style="display: grid;justify-content: center;width: 100%;">
-            <v-btn style="color: rgb(33, 150, 243) !important" :color="$blue" target="_blank" :href="'/storage/file-links/'+fileLink.file" text v-for="fileLink in fileLinks">
+            <v-btn style="color: rgb(33, 150, 243) !important" :color="$blue" target="_blank"
+                   :href="'/storage/file-links/'+fileLink.file" text v-for="fileLink in fileLinks"
+            >
               {{ fileLink.name }}
-            </v-btn><br>
+            </v-btn>
+            <br>
           </div>
           <div class="col-md-12 d-flex justify-content-center">
             <div class="d-flex">
@@ -168,6 +171,24 @@
               </template>
             </v-simple-table>
           </div>
+
+          <div class="p-3 row">
+            <div class="col-md-6 col-sm-12 justify-content-center" style="display: grid;">
+              <h5 class="text-center">Корт 1</h5>
+              <iframe src="http://www.cactus.tv:8080/cam24/embed.html" width="468" height="350" align="left">
+                Ваш браузер не поддерживает плавающие фреймы!
+              </iframe>
+            </div>
+            <div class="col-md-6 col-sm-12 justify6-content-center" style="display: grid;">
+              <h5 class="text-center">Корт 2</h5>
+              <iframe src="http://www.cactus.tv:8080/cam25/embed.html" width="468" height="350" align="left">
+                Ваш браузер не поддерживает плавающие фреймы!
+              </iframe>
+            </div>
+
+
+          </div>
+
         </div>
       </v-app>
       <v-dialog v-model="dialog" persistent max-width="300">
