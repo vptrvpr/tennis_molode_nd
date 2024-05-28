7f496a4ae57c67f0ce66894269bb9538973743b6
vptrvpr <vptrv.pr@gmail.com>
2024-05-29 00:57:14 +0300

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule( Schedule $schedule )
     {
         $schedule->command( 'update-hours' )
-                 ->mondays();
+                 ->mondays()->at( '00:00' );
     }
 
 
